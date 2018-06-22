0e83a826b3d9ee42ac774a66074c6771289f52fc
jakeparcell14 <jakeparcell14@gmail.com>
2018-06-21 23:19:47 -0700

    changed arrays to arraylists and added some methods

--- Factors and Factorials/src/FactorsAndFactorials.java
+++ Factors and Factorials/src/FactorsAndFactorials.java
@@ -13,6 +13,14 @@ public class FactorsAndFactorials
 	{
 		Scanner in = new Scanner(System.in);
 
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		findPrimes(primes);
+		
+		for(int i = 0; i < 25; i++)
+		{
+			System.out.println(primes.get(i));
+		}
+		
 		ArrayList<Integer> userInput = new ArrayList<Integer>();
 
 		while(true)
@@ -33,28 +41,54 @@ public class FactorsAndFactorials
 		}
 	}
 
-	public static ArrayList<Integer> findFactors(int number)
-	{
-		
-
+	public static ArrayList<Integer> findFactors(int number, ArrayList<Integer> primes)
+	{		
+		ArrayList<Integer> factors = new ArrayList<Integer>();
 
 		return null;
+	}
+	
+	/**
+	 * fills an array with all primes less than 100
+	 * @param primes
+	 */
+	public static void findPrimes(ArrayList<Integer> primes)
+	{
+		primes.add(2);
+		
+		// current index of the prime array
+		int count = 1;
+		
+		// possible prime number
+		int number = 3;
+		
+		while(count < 25)
+		{
+			// checks if a number is prime
+			if(isPrime(number))
+			{
+				// number is prime and is added to array
+				primes.add(number);
+				count++;
+			}
+			
+			number += 2;
+		}
 	}
 
 	/**
 	 * calculates if a given number is prime or not
 	 * @param number 			value to be tested
-	 * @param startingPoint		starting number to test for factors
 	 * @return					true if the number is prime, false if not
 	 */
-	public static boolean isPrime(int number, int startingPoint)
+	public static boolean isPrime(int number)
 	{
 		if(number % 2 == 0)
 		{
 			return false;
 		}
 
-		for(int i = startingPoint; i < number; i+= 2)
+		for(int i = 3; i < number; i+= 2)
 		{
 			if(number % i == 0)
 			{
